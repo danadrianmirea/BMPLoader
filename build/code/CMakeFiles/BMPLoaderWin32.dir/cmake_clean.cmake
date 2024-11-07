e17da0955fbf68c6478d1b50df646cd9075ce79c
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-07 08:44:38 +0200

    add build folder

---
+++ build/code/CMakeFiles/BMPLoaderWin32.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../BMPLoaderWin32.exe"
+  "../BMPLoaderWin32.exe.manifest"
+  "../BMPLoaderWin32.pdb"
+  "CMakeFiles/BMPLoaderWin32.dir/main.cpp.obj"
+  "CMakeFiles/BMPLoaderWin32.dir/main.cpp.obj.d"
+  "libBMPLoaderWin32.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BMPLoaderWin32.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
